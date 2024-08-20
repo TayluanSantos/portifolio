8e2f9d1a0f2d33b3eb535f978e9e952a03d1484e
TayluanSantos <tayluanjesus@gmail.com>
2024-08-20 17:00:46 -0300

    feat: Implementando novas validações ao formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,17 +36,50 @@ async function getApiGithub(){
 }
 formulario.addEventListener("submit",function(event){
     event.preventDefault(); // Não deixa enviar antes da validação
+
+    const campoNome = document.querySelector('#name');
+    const txtNome = document.querySelector('#txtNome');
+  
+    // Valida o campo name
+    if (campoNome.value.length < 3) {
+      txtNome.innerHTML = 'O Nome deve ter no minimo 3 caracteres.';
+      txtNome.style.color = "red"
+      txtNome.style.fontSize = "14px"
+      campoNome.focus();
+      return;
+    }else{
+      txtNome.innerHTML = '';
+    }
+
     const campoEmail = document.querySelector("#email")
     const txtEmail = document.querySelector("#txtEmail")
 
+    // Valida o campo email
     if(!campoEmail.value.match(emailRegex)){
         txtEmail.innerHTML = "Digite um e-mail válido"
         txtEmail.style.color = "red"
         txtEmail.style.fontSize = "14px"
         campoEmail.focus();
+        return;
     } else {
         txtEmail.innerHTML = ''
         formulario.submit();
     }
+
+    const campoSubject = document.querySelector('#subject');
+    const txtSubject = document.querySelector('#txtSubject');
+  
+     // Valida o campo subject
+    if (campoSubject.value.length < 5) {
+      txtSubject.innerHTML = 'O Assunto deve ter no minimo 5 caracteres.';
+      txtSubject.style.color = "red"
+      txtSubject.style.fontSize = "14px"
+      campoSubject.focus();
+      return;
+    }else{
+      txtSubject.innerHTML = '';
+    }
+
+    formulario.submit();
 })
 getApiGithub();
